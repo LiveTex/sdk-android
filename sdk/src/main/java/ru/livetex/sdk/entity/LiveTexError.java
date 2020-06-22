51f8f7d1a872ec2908d13b4df7c1e08ff3413931
Maxim Smirnov <maxim.smirnov@mercurydevelopment.com>
2020-06-22 16:47:46 +0400

    [SDK] Updated LiveTexError enum.

--- sdk/src/main/java/ru/livetex/sdk/entity/LiveTexError.java
+++ sdk/src/main/java/ru/livetex/sdk/entity/LiveTexError.java
@@ -3,8 +3,18 @@ package ru.livetex.sdk.entity;
 import com.google.gson.annotations.SerializedName;
 
 public enum LiveTexError {
+	@SerializedName("systemUnavailable")
+	SYSTEM_UNAVAILABLE, // server-side error
+	@SerializedName("departmentNoId")
+	NO_DEPARTMENT_ID,
 	@SerializedName("departmentInvalidId")
 	INVALID_DEPARTMENT,
+	@SerializedName("attributesWrongFormat")
+	ATTRIBUTES_WRONG_FORMAT, // wrong format of attributes HashMap in AttributesEntity
 	@SerializedName("textContentIsEmpty")
-	EMPTY_MESSAGE
+	EMPTY_MESSAGE,
+	@SerializedName("fileNoName")
+	FILE_NO_NAME,
+	@SerializedName("fileNoUrl")
+	FILE_NO_URL,
 }
