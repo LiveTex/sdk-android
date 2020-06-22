2c0dc656d9c3e3304462fe306408d5342d9f8528
Maxim Smirnov <maxim.smirnov@mercurydevelopment.com>
2020-06-22 12:57:43 +0400

    [Demo] Implemented date messages.

--- demo/src/main/java/ru/livetex/demoapp/utils/DateUtils.java
+++ demo/src/main/java/ru/livetex/demoapp/utils/DateUtils.java
@@ -1,8 +1,11 @@
 package ru.livetex.demoapp.utils;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
+
+import androidx.annotation.NonNull;
 
 public final class DateUtils {
 
@@ -16,6 +19,24 @@ public final class DateUtils {
 	}
 
 	public static synchronized String dateToDay(Date date) {
+		if (isToday(date.getTime())) {
+			return "Сегодня";
+		}
 		return sdfDay.format(date);
+	}
+
+	private static boolean isToday(long time) {
+		long todayStart = getDayStartForCalendar(System.currentTimeMillis(), Calendar.getInstance());
+		long todayEnd = todayStart + android.text.format.DateUtils.DAY_IN_MILLIS;
+		return time >= todayStart && time < todayEnd;
+	}
+
+	private static long getDayStartForCalendar(long originTime, @NonNull Calendar calendar) {
+		calendar.setTimeInMillis(originTime);
+		calendar.set(Calendar.HOUR_OF_DAY, 0);
+		calendar.set(Calendar.MINUTE, 0);
+		calendar.set(Calendar.SECOND, 0);
+		calendar.set(Calendar.MILLISECOND, 0);
+		return calendar.getTimeInMillis();
 	}
 }
