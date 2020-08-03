f074f8adb4869eb5343eb0b1a390c9e60f923a3e
Maxim Smirnov <maxim.smirnov@mercurydevelopment.com>
2020-08-03 20:55:27 +0400

    [SDK] Updated host address.

--- demo/src/main/java/ru/livetex/demoapp/Const.java
+++ demo/src/main/java/ru/livetex/demoapp/Const.java
@@ -1,7 +1,6 @@
 package ru.livetex.demoapp;
 
 public interface Const {
-	String HOST = "visitor-api-04.livetex.ru/";
 	String TOUCHPOINT = "168:58dc121d-9b8f-4d31-b142-7b11de675690";
 
 	String KEY_VISITOR_TOKEN = "KEY_VISITOR_TOKEN";
