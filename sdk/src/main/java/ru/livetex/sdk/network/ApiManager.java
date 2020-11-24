38f5c1f228b26c6af68726741c0b948d232fe596
xBlackCat <xblackcat@gmail.com>
2020-11-25 00:18:17 +0400

    Add possibility to upload a file to LiveTex specified by Uri ('file://' or 'content://' schemas) and by InputStream.
    Signed-off-by: maxxx <maxdestroyer@yandex.ru>

--- sdk/src/main/java/ru/livetex/sdk/network/ApiManager.java
+++ sdk/src/main/java/ru/livetex/sdk/network/ApiManager.java
@@ -1,12 +1,24 @@
 package ru.livetex.sdk.network;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.ParcelFileDescriptor;
+import android.provider.OpenableColumns;
+import android.webkit.MimeTypeMap;
+
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import io.reactivex.Single;
+import io.reactivex.SingleEmitter;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.MediaType;
@@ -14,6 +26,8 @@ import okhttp3.MultipartBody;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
+import okio.BufferedSink;
+import okio.Okio;
 import ru.livetex.sdk.entity.FileUploadedResponse;
 
 /**
@@ -21,6 +35,7 @@ import ru.livetex.sdk.entity.FileUploadedResponse;
  */
 public final class ApiManager {
 
+	private final Gson gson = new GsonBuilder().create();
 	private final OkHttpManager okHttpManager;
 	private String authToken = null;
 
@@ -57,12 +72,12 @@ public final class ApiManager {
 			try {
 				okHttpManager.getClient().newCall(request).enqueue(new Callback() {
 					@Override
-					public void onFailure(final Call call, final IOException e) {
+					public void onFailure(@NonNull final Call call, @NonNull final IOException e) {
 						emitter.tryOnError(e);
 					}
 
 					@Override
-					public void onResponse(final Call call, final Response response) {
+					public void onResponse(@NonNull final Call call, @NonNull final Response response) {
 						if (!response.isSuccessful()) {
 							// todo: something better? need error handling
 							emitter.tryOnError(new IOException("response is " + response));
@@ -70,7 +85,6 @@ public final class ApiManager {
 						}
 						try {
 							String string = response.body().string();
-							Gson gson = new GsonBuilder().create();
 							FileUploadedResponse fResp = gson.fromJson(string, FileUploadedResponse.class);
 							emitter.onSuccess(fResp);
 						} catch (IOException e) {
@@ -82,5 +96,142 @@ public final class ApiManager {
 				emitter.tryOnError(ex);
 			}
 		});
+	}
+
+	public Single<FileUploadedResponse> uploadFile(Context context, Uri uri) {
+		return Single.create(emitter -> {
+			if (authToken == null) {
+				emitter.tryOnError(new IllegalStateException("uploadFile called with null auth token"));
+				return;
+			}
+
+			ParcelFileDescriptor pfd;
+			try {
+				pfd = context.getContentResolver().openFileDescriptor(uri, "r");
+			} catch (Exception e) {
+				emitter.tryOnError(e);
+				return;
+			}
+
+			if (pfd == null) {
+				emitter.tryOnError(new IOException("failed to open file"));
+				return;
+			}
+
+			String fileName = null;
+			if (Objects.equals(uri.getScheme(), "content")) {
+				final Cursor cursor = context.getContentResolver().query(uri, null, null, null, null);
+				if (cursor != null) {
+					try {
+						if (cursor.moveToFirst()) {
+							fileName = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+						}
+					} finally {
+						cursor.close();
+					}
+				}
+			}
+
+			if (fileName == null) {
+				final String path = uri.getPath();
+				if (path != null) {
+					int pos = path.lastIndexOf('/');
+					fileName = pos >= 0 ? path.substring(pos + 1) : path;
+				}
+			}
+
+			String extension = null;
+			if (fileName != null) {
+				int pos = fileName.lastIndexOf('.');
+				extension = pos >= 0 ? fileName.substring(pos + 1) : null;
+			}
+
+			String mimeType = null;
+			if (extension != null) {
+				mimeType = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
+			}
+
+			MediaType mediaType = MediaType.parse(mimeType != null ? mimeType : "text/plain");
+
+			InputStream inputStream;
+			try {
+				inputStream = context.getContentResolver().openInputStream(uri);
+			} catch (Exception e) {
+				emitter.tryOnError(e);
+				return;
+			}
+
+			uploadFile(fileName, inputStream, mediaType, emitter);
+		});
+	}
+
+	public Single<FileUploadedResponse> uploadFile(String fileName, InputStream inputStream, @Nullable MediaType mediaType) {
+		return Single.create(emitter -> {
+			if (authToken == null) {
+				emitter.tryOnError(new IllegalStateException("uploadFile called with null auth token"));
+				return;
+			}
+
+			uploadFile(fileName, inputStream, mediaType, emitter);
+		});
+	}
+
+	private void uploadFile(
+			String fileName,
+			InputStream inputStream,
+			@Nullable MediaType mediaType,
+			SingleEmitter<FileUploadedResponse> emitter
+	) {
+		RequestBody requestBody = new MultipartBody.Builder()
+				.setType(MultipartBody.FORM)
+				.addFormDataPart(
+						"fileUpload",
+						fileName,
+						new RequestBody() {
+							@Override
+							public MediaType contentType() {
+								return mediaType != null ? mediaType : MediaType.parse("text/plain");
+							}
+
+							@Override
+							public void writeTo(@NonNull BufferedSink sink) throws IOException {
+								sink.writeAll(Okio.source(inputStream));
+							}
+						}
+				)
+				.build();
+
+		Request request = new Request.Builder()
+				.addHeader("Authorization", "Bearer " + authToken)
+				.url(NetworkManager.getInstance().getUploadEndpoint())
+				.post(requestBody)
+				.build();
+
+		try {
+			okHttpManager.getClient().newCall(request).enqueue(new Callback() {
+				@Override
+				public void onFailure(@NonNull final Call call, @NonNull final IOException e) {
+					emitter.tryOnError(e);
+				}
+
+				@Override
+				public void onResponse(@NonNull final Call call, @NonNull final Response response) {
+					if (!response.isSuccessful()) {
+						// todo: something better? need error handling
+						emitter.tryOnError(new IOException("response is " + response));
+						return;
+					}
+					try {
+						String string = response.body().string();
+						FileUploadedResponse fResp = gson.fromJson(string, FileUploadedResponse.class);
+						emitter.onSuccess(fResp);
+					} catch (IOException e) {
+						emitter.tryOnError(e);
+					}
+				}
+			});
+		} catch (Exception ex) {
+			emitter.tryOnError(ex);
+		}
 	}
 }
