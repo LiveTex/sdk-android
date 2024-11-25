ed1d1b1d6de6078351a80cd895f131e467f595fa
Maxim Smirnov <maxdestroyer@yandex.ru>
2024-11-25 22:10:31 +0400

    Added new fields for rating system

--- sdk/src/main/java/ru/livetex/sdk/entity/RatingEvent.java
+++ sdk/src/main/java/ru/livetex/sdk/entity/RatingEvent.java
@@ -1,5 +1,7 @@
 package ru.livetex.sdk.entity;
 
+import androidx.annotation.Nullable;
+
 public final class RatingEvent extends BaseEntity {
 	public static final String TYPE = "rating";
 
@@ -10,14 +12,18 @@ public final class RatingEvent extends BaseEntity {
 	}
 
 	public RatingEvent(DialogRatingType type, String rating) {
-		this.rate = new DialogRatingData(type, rating);
+		this.rate = new DialogRatingData(type, rating, null);
+	}
+
+	public RatingEvent(DialogRatingType type, String rating, String comment) {
+		this.rate = new DialogRatingData(type, rating, comment);
 	}
 
 	/**
 	 * old system
 	 */
 	public static RatingEvent createEvent2point(boolean isPositiveFeedback) {
-		return new RatingEvent(new DialogRatingData(DialogRatingType.DOUBLE_POINT, isPositiveFeedback ? "1" : "0"));
+		return new RatingEvent(new DialogRatingData(DialogRatingType.DOUBLE_POINT, isPositiveFeedback ? "1" : "0", null));
 	}
 
 	/**
@@ -25,7 +31,15 @@ public final class RatingEvent extends BaseEntity {
 	 * @param rating from 1 to 5
 	 */
 	public static RatingEvent createEvent5points(short rating) {
-		return new RatingEvent(new DialogRatingData(DialogRatingType.FIVE_POINT, Short.toString(rating)));
+		return new RatingEvent(new DialogRatingData(DialogRatingType.FIVE_POINT, Short.toString(rating), null));
+	}
+
+	/**
+	 * new system
+	 * @param rating from 1 to 5
+	 */
+	public static RatingEvent createEvent5points(short rating, @Nullable String comment) {
+		return new RatingEvent(new DialogRatingData(DialogRatingType.FIVE_POINT, Short.toString(rating), comment));
 	}
 
 	@Override
